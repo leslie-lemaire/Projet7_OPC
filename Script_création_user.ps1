b0c7fe443f69028133c7cc9617d57711f116f204
leslie-lemaire <leslie.lemaire@diiage.org>
2024-07-16 21:36:23 +0200

    Mise en place du script

--- Script_création_user.ps1
+++ Script_création_user.ps1
@@ -1,1 +1,61 @@
-dsqd
+# Charger le module Active Directory
+Import-Module ActiveDirectory
+
+# Fonction pour demander les informations de l'utilisateur
+function Get-UserInfo {
+    $user = @{}
+    $user.FirstName = Read-Host "Prénom de l'utilisateur"
+    $user.LastName = Read-Host "Nom de l'utilisateur"
+    $user.Username = Read-Host "Nom d'utilisateur (login)"
+    $user.Password = Read-Host -AsSecureString "Mot de passe"
+    $user.OU = Read-Host "OU (par exemple: 'OU=Users,DC=domain,DC=com')"
+    $user.HomeDirectory = Read-Host "Chemin du dossier personnel (par exemple: '\\fileserver\home\username')"
+    return $user
+}
+
+# Fonction pour créer l'utilisateur AD
+function Create-ADUser {
+    param (
+        [Parameter(Mandatory=$true)] $user
+    )
+    New-ADUser -Name "$($user.FirstName) $($user.LastName)" `
+               -GivenName $user.FirstName `
+               -Surname $user.LastName `
+               -SamAccountName $user.Username `
+               -UserPrincipalName "$($user.Username)@domain.com" `
+               -Path $user.OU `
+               -AccountPassword $user.Password `
+               -Enabled $true `
+               -HomeDirectory $user.HomeDirectory `
+               -HomeDrive "H:" `
+               -PassThru
+}
+
+# Fonction pour configurer le dossier partagé personnel
+function Configure-HomeDirectory {
+    param (
+        [Parameter(Mandatory=$true)] $user
+    )
+    $path = $user.HomeDirectory
+    $username = $user.Username
+    New-Item -Path $path -ItemType Directory -Force
+
+    # Configurer les permissions
+    $acl = Get-Acl $path
+    $acl.SetAccessRule((New-Object System.Security.AccessControl.FileSystemAccessRule("$username", "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow")))
+    Set-Acl $path $acl
+
+    # Partager le dossier
+    New-SmbShare -Name $username -Path $path -FullAccess "$username"
+}
+
+# Demander les informations de l'utilisateur
+$userInfo = Get-UserInfo
+
+# Créer l'utilisateur AD
+$user = Create-ADUser -user $userInfo
+
+# Configurer le dossier partagé personnel
+Configure-HomeDirectory -user $userInfo
+
+Write-Host "Utilisateur et dossier personnel créés avec succès."
