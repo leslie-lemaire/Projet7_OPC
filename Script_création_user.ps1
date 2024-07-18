16406f91e8f6b263930632c1b07be576fc2a2970
leslie-lemaire <leslie.lemaire@diiage.org>
2024-07-18 20:40:35 +0200

    dsq

--- Script_création_user.ps1
+++ Script_création_user.ps1
@@ -16,8 +16,8 @@ function Get-UserInfo {
     $user.LastName = Read-Host "Nom de l'utilisateur"
     $user.Username = Read-Host "Nom d'utilisateur (login)"
     $user.Password = Read-Host -AsSecureString "Mot de passe"
-    $user.OU = Read-Host "OU (par exemple: 'OU=Users,DC=axeplane,DC=loc')"
-    $user.HomeDirectory = Read-Host "Chemin du dossier perso"
+    $user.OU = Read-Host "Chemin de l'OU (par exemple: 'OU=Users,DC=axeplane,DC=loc')"
+    $user.HomeDirectory = Read-Host "Chemin du dossier personnel (par exemple: '\\WIN-90LDUDNTQDE\Partages personnels utilisateurs\username')"
     return $user
 }
 
@@ -26,17 +26,22 @@ function Create-ADUser {
     param (
         [Parameter(Mandatory=$true)] $user
     )
-    New-ADUser -Name "$($user.FirstName) $($user.LastName)" `
-               -GivenName $user.FirstName `
-               -Surname $user.LastName `
-               -SamAccountName $user.Username `
-               -UserPrincipalName "$($user.Username)@axeplane.loc" `
-               -Path $user.OU `
-               -AccountPassword $user.Password `
-               -Enabled $true `
-               -HomeDirectory $user.HomeDirectory `
-               -HomeDrive "H:" `
-               -PassThru
+    try {
+        New-ADUser -Name "$($user.FirstName) $($user.LastName)" `
+                   -GivenName $user.FirstName `
+                   -Surname $user.LastName `
+                   -SamAccountName $user.Username `
+                   -UserPrincipalName "$($user.Username)@axeplane.loc" `
+                   -Path $user.OU `
+                   -AccountPassword $user.Password `
+                   -Enabled $true `
+                   -HomeDirectory $user.HomeDirectory `
+                   -HomeDrive "H:" `
+                   -PassThru
+    } catch {
+        Write-Host "Erreur lors de la création de l'utilisateur : $_"
+        exit 1
+    }
 }
 
 # Fonction pour configurer le dossier partagé personnel
@@ -44,17 +49,27 @@ function Configure-HomeDirectory {
     param (
         [Parameter(Mandatory=$true)] $user
     )
-    $path = $user.HomeDirectory
+    $path = $user.HomeDirectory -replace 'username', $user.Username
     $username = $user.Username
-    New-Item -Path $path -ItemType Directory -Force
+    
+    try {
+        # Créer le dossier s'il n'existe pas
+        if (-Not (Test-Path -Path $path)) {
+            New-Item -Path $path -ItemType Directory -Force
+        }
 
-    # Configurer les permissions
-    $acl = Get-Acl $path
-    $acl.SetAccessRule((New-Object System.Security.AccessControl.FileSystemAccessRule("$username", "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow")))
-    Set-Acl $path $acl
+        # Configurer les permissions
+        $acl = Get-Acl $path
+        $accessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("$username", "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow")
+        $acl.SetAccessRule($accessRule)
+        Set-Acl $path $acl
 
-    # Partager le dossier
-    New-SmbShare -Name $username -Path $path -FullAccess "$username"
+        # Partager le dossier
+        New-SmbShare -Name $username -Path $path -FullAccess "$username"
+    } catch {
+        Write-Host "Erreur lors de la configuration du dossier partagé : $_"
+        exit 1
+    }
 }
 
 # Demander les informations de l'utilisateur
